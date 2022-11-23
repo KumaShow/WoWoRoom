c41cd2d94d11b97f460578bc6c4799b9bdada4ed
KumaShow <kumadth@gmail.com>
2022-11-24 00:17:40 +0800

    Update 2022-11-23T16:17:30.753Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,50 +1,53 @@
-const api_path = "js-2022";
-const urlProducts = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/products`;
+"use strict";
 
-axios
-  .get(urlProducts)
-  .then((res) => {
-    console.log(res.data);
-  })
-  .catch((err) => {
-    console.log(err.data);
-  });
-
+var api_path = "js-2022";
+var urlProducts = "https://livejs-api.hexschool.io/api/livejs/v1/customer/".concat(api_path, "/products");
+axios.get(urlProducts).then(function (res) {
+  console.log(res.data);
+})["catch"](function (err) {
+  console.log(err.data);
+});
 document.addEventListener("DOMContentLoaded", function () {
-  const ele = document.querySelector(".review-wall");
+  var ele = document.querySelector(".review-wall");
   ele.style.cursor = "grab";
-  let pos = { top: 0, left: 0, x: 0, y: 0 };
-  const mouseDownHandler = function (e) {
+  var pos = {
+    top: 0,
+    left: 0,
+    x: 0,
+    y: 0
+  };
+
+  var mouseDownHandler = function mouseDownHandler(e) {
     ele.style.cursor = "grabbing";
     ele.style.userSelect = "none";
-
     pos = {
       left: ele.scrollLeft,
       top: ele.scrollTop,
       // Get the current mouse position
       x: e.clientX,
-      y: e.clientY,
+      y: e.clientY
     };
-
     document.addEventListener("mousemove", mouseMoveHandler);
     document.addEventListener("mouseup", mouseUpHandler);
   };
-  const mouseMoveHandler = function (e) {
-    // How far the mouse has been moved
-    const dx = e.clientX - pos.x;
-    const dy = e.clientY - pos.y;
 
-    // Scroll the element
+  var mouseMoveHandler = function mouseMoveHandler(e) {
+    // How far the mouse has been moved
+    var dx = e.clientX - pos.x;
+    var dy = e.clientY - pos.y; // Scroll the element
+
     ele.scrollTop = pos.top - dy;
     ele.scrollLeft = pos.left - dx;
   };
-  const mouseUpHandler = function () {
+
+  var mouseUpHandler = function mouseUpHandler() {
     ele.style.cursor = "grab";
     ele.style.removeProperty("user-select");
-
     document.removeEventListener("mousemove", mouseMoveHandler);
     document.removeEventListener("mouseup", mouseUpHandler);
-  };
-  // Attach the handler
+  }; // Attach the handler
+
+
   ele.addEventListener("mousedown", mouseDownHandler);
 });
+//# sourceMappingURL=all.js.map
