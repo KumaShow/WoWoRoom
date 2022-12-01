e54c79325d6523565c6024af4587294529b6f8af
KumaShow <kumadth@gmail.com>
2022-12-01 22:36:24 +0800

    更新前台js
    1. 優化程式碼
    2. 新增產品篩選
    3. 新增頁面切換觸發不同初始化功能

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,15 +1,13 @@
-const api_path = "js-2022";
 const urlProducts = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/products`;
 const urlCarts = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`;
-
 const productList = document.querySelector('#productList');
 const form = document.querySelector('#orderForm');
 const inputs = document.querySelectorAll("[data-validate]");
 const messages = document.querySelectorAll("[data-msg]");
 
-// let pathName = location.pathname;
-// if(pathName.includes('index')) {}
-
+let productListData;
+let productCategory = {};
+let cartsData;
 
 // TODO: 驗證套件
 /*
@@ -68,17 +66,40 @@ inputs.forEach((item) => {
 });
 */
 
+// 不同頁面分別執行初始化
+function locationPathChanged() {
+  if (location.pathname.includes('index')) {
+    // 首頁初始化
+    function init() {
+      getProducts();
+      getCarts();
+      recommendationDrag();
+      
+    }
+    init();
+  } else {
+    // 後臺初始化
+    function initAdmin() {
+      getOrders();
+      orderListClick();
+    }
+    initAdmin();
+  }
+}
+locationPathChanged();
 
 // 表單送出監聽
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  // console.log(errors);
-  // if (errors) {
-  //   showErrorMessage(errors);
-  // } else {
-    addOrder();
-  // }
-});
+function submitForm() {
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    // console.log(errors);
+    // if (errors) {
+    //   showErrorMessage(errors);
+    // } else {
+      addOrder();
+    // }
+  });
+}
 
 // 送出訂單
 function addOrder() {
@@ -102,24 +123,48 @@ function addOrder() {
   }
 
   axios.post(urlOrder,order)
-    .then(res => {
+    .then(() => {
       form.reset();
       getCarts();
-      alert('已送出訂單');
+      swalAddOrderSuccess();
     })
-    .catch(err => console.log('提交訂單失敗，請重新確認'));
+    .catch(() => {
+      swalAddOrderError();
+    });
 }
 
 // 取得產品列表
 function getProducts() {
   axios.get(urlProducts)
   .then((res) => {
-    const productListData = res.data.products;
+    productListData = res.data.products;
     renderProductList(productListData);
+    productListClick();
+    productFilterChange(productListData);
   })
   .catch((err) => {
     console.log(err);
+    console.log('產品列表沒有資料');
   });
+}
+
+// 產品篩選
+function productFilterChange() {
+  const productType = document.querySelector('#productType');
+
+  productType.addEventListener('change', e => {
+    const category = e.target.value;
+
+    if(category === '全部') {
+      renderProductList(productListData);
+      return;
+    }
+
+    const filterArr = productListData.filter(item => {
+      return item.category === category;
+    });
+    renderProductList(filterArr);
+  })
 }
 
 // 渲染產品列表
@@ -144,16 +189,16 @@ function renderProductList(productListData) {
   productList.innerHTML = str;
 }
 
-let cartsData;
 // 加入購物車
-productList.addEventListener('click', e => {
-  if(e.target.nodeName !== 'BUTTON') {
-    return;
-  }
-  const productId = e.target.dataset.id;
-
-  addToCarts(productId);
-})
+function productListClick() {
+  productList.addEventListener('click', e => {
+    if(e.target.nodeName !== 'BUTTON') {
+      return;
+    }
+    const productId = e.target.dataset.id;
+    addToCarts(productId);
+  })
+}
 
 // 取得購物車資料
 function getCarts() {
@@ -162,8 +207,8 @@ function getCarts() {
       cartsData = res.data.carts;
       renderCarts(cartsData);
     })
-    .catch(err => {
-      console.log(err);
+    .catch(() => {
+      // console.log('購物車沒有資料');
     });
 }
 
@@ -181,6 +226,7 @@ function renderCarts(cartsData) {
       </tr>
     `;
   } else {
+    const cartsList = document.querySelector('#cartsList');
     thead.classList.remove('d-none');
     cartsData.forEach(item => {
       total += item.quantity * item.product.price;
@@ -194,10 +240,9 @@ function renderCarts(cartsData) {
           <td>${item.quantity}</td>
           <td>NT$${item.product.price * item.quantity}</td>
           <td class="text-end">
-            <button type="button" class="border-0 bg-transparent" data-id="${item.id}" 
-            onClick="deleteCartsItem('${item.id}')">
-              <i class="fa-solid fa-x"></i>
-            </button>
+            <a href="#!" class="border-0 bg-transparent link-black" data-id="${item.id}">
+              <i class="fa-solid fa-x" data-id="${item.id}"></i>
+            </a>
           </td>
         </tr>
       `;
@@ -206,7 +251,7 @@ function renderCarts(cartsData) {
     str += `
       <tr class="carts-total">
         <td class="border-0">
-          <button type="button" class="btn btn-outline-black py-3 px-6" onclick="deleteAllCarts()">刪除所有品項</button>
+          <button type="button" class="btn btn-outline-black py-3 px-6" id="btnClearCarts">刪除所有品項</button>
         </td>
         <td class="border-0"></td>
         <td class="border-0"></td>
@@ -217,6 +262,8 @@ function renderCarts(cartsData) {
       </tr>
     `
     cartsList.innerHTML = str;
+    btnClearCartsClick();
+    cartsListClick();
   }
 }
 
@@ -236,43 +283,106 @@ function addToCarts(id) {
   };
 
   axios.post(urlCarts, data)
-  .then(res => {
+  .then(() => {
     getCarts();
-    console.log('已加入購物車');
-    // alert("已加入購物車");
+    swalCartsSuccess();
   })
-  .catch(err => console.log(err));
+  .catch(() => {
+    swalCartsError();
+  });
 }
 
-// 刪除購物車單筆(設置在 dom onclick)
+// 綁定清除購物車按鈕
+function btnClearCartsClick() {
+  const btnClearCarts = document.querySelector('#btnClearCarts');
+  btnClearCarts.addEventListener('click', e => {
+    e.preventDefault();
+    deleteAllCarts();
+  });
+}
+
+// 購物車刪除事件
+function cartsListClick() {
+  cartsList.addEventListener('click', e => {
+    e.preventDefault();
+    const cartItemId = e.target.getAttribute('data-id');
+  
+    if(cartItemId === null) {
+      return;
+    } else {
+      deleteCartsItem(cartItemId);
+    }
+  });
+}
+
+// 刪除購物車單筆
 function deleteCartsItem(id) {
   const url = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts/${id}`;
 
   axios.delete(url,id)
-    .then(res => {
+    .then(() => {
       getCarts();
     })
-    .catch(err => {
+    .catch(() => {
       console.log('沒有這筆資料');
     })
 }
 
-// 刪除購物車所有品項(設置在 dom onclick)
+// 刪除購物車所有品項
 function deleteAllCarts() {
   axios.delete(urlCarts)
-    .then(res => {
-      alert('購物車已清空');
+    .then(() => {
+      swalDeleteCartsSuccess();
       getCarts();
     })
-    .catch(err => {
-      console.log(err);
+    .catch(() => {
+      swalDeleteCartsError();
     });
 }
 
-
-function init() {
-  getProducts();
-  getCarts();
+// 好評推薦拖拉功能
+function recommendationDrag() {
+  document.addEventListener("DOMContentLoaded", function () {
+    const ele = document.querySelector(".recommendation-wall");
+    ele.style.cursor = "grab";
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+    const mouseDownHandler = function (e) {
+      ele.style.cursor = "grabbing";
+      ele.style.userSelect = "none";
+  
+      pos = {
+        left: ele.scrollLeft,
+        top: ele.scrollTop,
+        // Get the current mouse position
+        x: e.clientX,
+        y: e.clientY,
+      };
+  
+      document.addEventListener("mousemove", mouseMoveHandler);
+      document.addEventListener("mouseup", mouseUpHandler);
+    };
+    const mouseMoveHandler = function (e) {
+      // How far the mouse has been moved
+      const dx = e.clientX - pos.x;
+      const dy = e.clientY - pos.y;
+  
+      // Scroll the element
+      ele.scrollTop = pos.top - dy;
+      ele.scrollLeft = pos.left - dx;
+    };
+    const mouseUpHandler = function () {
+      ele.style.cursor = "grab";
+      ele.style.removeProperty("user-select");
+  
+      document.removeEventListener("mousemove", mouseMoveHandler);
+      document.removeEventListener("mouseup", mouseUpHandler);
+    };
+    // Attach the handler
+    ele.addEventListener("mousedown", mouseDownHandler);
+  });
 }
-init();
 
+
+/* TODO:
+  2. 驗證套件
+*/
