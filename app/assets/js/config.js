7741ee32c3bd061fe3507308c121207d562ef31f
KumaShow <kumadth@gmail.com>
2022-12-03 00:21:59 +0800

    Update config.js

--- app/assets/js/config.js
+++ app/assets/js/config.js
@@ -1,25 +1,2 @@
 const api_path = "js-2022";
 const token = "kk6wTiHvysgMHTb81Mkv3q2j0M23";
-
-// 不同頁面分別執行初始化
-function locationPathChanged() {
-  if (location.pathname.includes('index') || 
-      location.pathname === '/') {
-    // 首頁初始化
-    function init() {
-      getProducts();
-      getCarts();
-      recommendationDrag();
-      submitForm();
-    }
-    init();
-  } else if(location.pathname.includes('admin')) {
-    // 後臺初始化
-    function initAdmin() {
-      getOrders();
-      orderListClick();
-    }
-    initAdmin();
-  }
-}
-locationPathChanged();
